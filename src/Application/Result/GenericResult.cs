15be018f4bab5d3c1a3877b3459a21f1a2fc1f77
Bilal <billelmh501@gmail.com>
2025-08-28 22:47:50 +0100

    added the ResultPattern

---
+++ src/Application/Result/GenericResult.cs
@@ -0,0 +1,31 @@
+﻿using Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Result
+{
+    public class Result<T> :
+        Result where T : class
+    {
+        public T? Value { get; }
+
+        private Result(bool isSuccess, T value, string error, ErrorType errorType = ErrorType.NotFound)
+            : base(isSuccess, error, errorType)
+        {
+            Value = value;
+        }
+
+        public static new Result<T> Success(T value) => new Result<T>(true, value, null!);
+        public static new Result<T> Failure(string error, ErrorType errorType)
+            => new Result<T>(false, default!, error, errorType);
+        public static new Result<T> NotFound(string entity)
+        {
+            string errorMessage = $"{entity} nor found";
+            return Failure(errorMessage, ErrorType.NotFound);
+        }
+
+    }
+}
---
+++ src/Application/Result/Result.cs
@@ -0,0 +1,34 @@
+﻿using Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Result
+{
+    public class Result
+    {
+        public bool IsSuccess { get; protected set; }
+        public string? ErrorMessage { get; protected set; }
+        public ErrorType ErrorType { get; protected set; }
+
+        protected Result(bool isSuccess, string? errorMessage = null, ErrorType errorType = ErrorType.InternalServerError)
+        {
+            IsSuccess = isSuccess;
+            ErrorMessage = errorMessage;
+            ErrorType = errorType;
+        }
+        public static Result Success => new Result(true);
+        public static Result Failure(string errorMessage,
+            ErrorType errorType = ErrorType.InternalServerError)
+        {
+            return new Result(false, errorMessage, errorType);
+        }
+        public static Result NotFound(string entity)
+        {
+            string errorMessage = $"{entity} nor found";
+            return Failure(errorMessage, ErrorType.NotFound);
+        }
+    }
+}
