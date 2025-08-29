553ff8d4e991e5d743935f0b5aed43f82ed3562c
Bilal <billelmh501@gmail.com>
2025-08-29 14:28:38 +0100

    feat(add the login feature

---
+++ src/Infrastructure/Authentication/Argon2PasswordHasher.cs
@@ -0,0 +1,23 @@
+﻿using Application.Common.Abstractions;
+using Isopoh.Cryptography.Argon2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Common
+{
+    public class Argon2PasswordHasher : IPasswordHasher
+    {
+        public string HashPassword(string password)
+        {
+            return Argon2.Hash(password);
+        }
+
+        public bool VerifyPassword(string hashedPassword, string providedPassword)
+        {
+            return Argon2.Verify(hashedPassword, providedPassword);
+        }
+    }
+}
---
+++ src/Presentation/Controllers/Auth/AuthenticationController.cs
@@ -0,0 +1,27 @@
+﻿using Application.Abstractions.Services;
+using Application.DTOs.Authentication;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Presentation.Controllers.Auth
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+        private readonly IAuthenticationService _service;
+
+        public AuthenticationController(IAuthenticationService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet("login")]
+        public async Task<ActionResult<LoginResponse>> LoginAsync(
+            LoginRequest loginRequest,CancellationToken cancellationToken)
+        {
+            var response= await _service.LoginAsync(loginRequest,cancellationToken);
+            return Ok(response);
+        }
+    }
+}
