f35f86881a7b2b3f9b07e256c32263a9269d3a96
Bilal <billelmh501@gmail.com>
2025-08-30 12:02:52 +0100

    implement and register ICurrentUserService

---
+++ src/Application/Abstractions/Services/User/ICurrentUserService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Abstractions.Services.User
+{
+    public interface ICurrentUserService
+    {
+        int UserId { get; }
+        
+    }
+}
---
+++ src/Infrastructure/Services/CurrentUserService.cs
@@ -0,0 +1,31 @@
+﻿using Application.Abstractions.Services.User;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Services
+{
+    internal class CurrentUserService : ICurrentUserService
+    {
+        private readonly IHttpContextAccessor _contextAccessor;
+
+        public CurrentUserService(IHttpContextAccessor contextAccessor)
+        {
+            _contextAccessor = contextAccessor;
+        }
+
+        public int UserId
+        {
+            get
+            {
+                var userId = _contextAccessor.HttpContext?.User?
+                    .FindFirst(ClaimTypes.NameIdentifier)?.Value;
+                return int.TryParse(userId, out int id) ? id : -1;
+            }
+        }
+    }
+}
