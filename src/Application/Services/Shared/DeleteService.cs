6f50e4e44dea112f517b05a7ba2294ccd479d3bc
Bilal <billelmh501@gmail.com>
2025-09-03 21:58:48 +0100

    feat(implement the enpoints for the user-roles with unit tests for the UserRoleService)

---
+++ src/Application/Services/Shared/DeleteService.cs
@@ -0,0 +1,63 @@
+﻿using Application.Abstractions.Repositories.Base;
+using Application.Abstractions.Services.User;
+using Application.Abstractions.UnitOfWork;
+using Application.Results;
+using Domain.Abstractions;
+using Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Services.Shared
+{
+    public abstract class DeleteService<TEntity> 
+        where TEntity : class,IEntity,ISoftDeletable
+    {
+        protected readonly IBaseRepository<TEntity> _repository;
+        protected readonly ICurrentUserService _currentUserService;
+        protected readonly IUnitOfWork _uow;
+        public DeleteService(IBaseRepository<TEntity> repository, ICurrentUserService currentUserService, IUnitOfWork uow)
+        {
+            _repository = repository;
+            _currentUserService = currentUserService;
+            _uow = uow;
+        }
+
+        public virtual async Task<Result> SoftDeleteAsync(int id
+            ,CancellationToken cancellationToken)
+        {
+            if(id <= 0)
+            {
+                return Result.InvalidId();
+            }
+            try
+            {
+                var entity = await _repository.FindAsync(e => e.Id == id
+            , cancellationToken);
+                if (entity is null)
+                {
+                    return Result.NotFound(typeof(TEntity).Name);
+                }
+                if (entity.IsDeleted)
+                {
+                    string errorMessage = $"{typeof(TEntity).Name} is already deleted";
+                    return Result.Failure(errorMessage, ErrorType.BadRequest);
+                }
+                entity.IsDeleted = true;
+                entity.DeletedAt = DateTime.UtcNow;
+                entity.DeletedByUserId = _currentUserService.UserId;
+                _repository.Update(entity);
+                await _uow.SaveChangesAsync(cancellationToken);
+                return Result.Success;
+            }
+            catch(Exception ex)
+            {
+                return Result.Failure($"Error:{ex.Message}"
+                    , ErrorType.InternalServerError);
+            }
+        }
+    }
+}
