2bc3cd4fefb4c65bab4dfe8861ba18fd58865d23
Bilal <billelmh501@gmail.com>
2025-09-04 23:36:46 +0100

    feat(create the Dtos and fluent validation for the UnitOfMeasure feature

---
+++ src/Application/DTOs/UnitOfMeasure/UnitOfMeasureRequest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.DTOs.UnitOfMeasure
+{
+    public sealed record UnitOfMeasureRequest
+    {
+        public string Name { get; init; } = string.Empty;
+        public string? Description { get; init; } = string.Empty;
+    }
+}
