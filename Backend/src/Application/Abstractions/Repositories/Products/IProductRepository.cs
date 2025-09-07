fecf4dff13ed3027f0ca60a1476506e3e9dca1ba
Bilal <billelmh501@gmail.com>
2025-09-08 00:02:37 +0100

    fix folder dir and fix conflicts

---
+++ Backend/src/Application/Abstractions/Repositories/Products/IProductRepository.cs
@@ -0,0 +1,14 @@
+﻿using Application.Abstractions.Repositories.Base;
+using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Abstractions.Repositories.Products
+{
+    public interface IProductRepository : IBaseRepository<Product>
+    {
+    }
+}
---
+++ Backend/src/Application/FluentValidations/Product/ProductValidatorContainer.cs
@@ -0,0 +1,21 @@
+﻿using Application.DTOs.Products.Request.Products;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.FluentValidations.Products
+{
+    public class ProductValidatorContainer
+    {
+        public IValidator<ProductCreateRequest> CreateValidator { get; }
+        public IValidator<ProductUpdateRequest> UpdateValidator { get; }
+        public ProductValidatorContainer(IValidator<ProductCreateRequest> createValidator, IValidator<ProductUpdateRequest> updateValidator)
+        {
+            CreateValidator = createValidator;
+            UpdateValidator = updateValidator;
+        }
+    }
+}
