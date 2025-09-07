36f0780964faa269f3b7b4a85ddde98e8096dd64
Bilal <billelmh501@gmail.com>
2025-09-07 21:35:02 +0100

    change the folders dir and wrap and api in a backend folder

---
+++ Backend/Tests/Tests.console/Program.cs
@@ -0,0 +1,20 @@
+﻿using Isopoh.Cryptography.Argon2;
+
+
+internal class Program
+{
+
+    public  static string HashPassword(string password)
+    {
+        return Argon2.Hash(password);
+    }
+    public static bool VerifyPassword(string hashedPassword, string providedPassword)
+    {
+        return Argon2.Verify(hashedPassword, providedPassword);
+    }
+    public static void Main(string[] args)
+    {
+        Console.WriteLine(HashPassword("1234"));
+        Console.WriteLine(VerifyPassword("ok", "1234"));
+    }
+}
