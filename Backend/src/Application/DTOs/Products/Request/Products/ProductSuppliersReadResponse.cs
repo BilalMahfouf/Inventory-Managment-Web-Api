90ba4cc99acfba25e71698ced6ca6b070042b332
Bilal <billelmh501@gmail.com>
2025-09-09 20:04:25 +0100

    feat(create DTOs  for the product-suppliers and low-stock end points)

---
+++ Backend/src/Application/DTOs/Products/Request/Products/ProductSuppliersReadResponse.cs
@@ -0,0 +1,25 @@
+﻿using Application.DTOs.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.DTOs.Products.Request.Products
+{
+    public sealed record ProductSuppliersReadResponse : BaseDeletableReadResponse
+    {
+        public int ProductId { get; init ; }
+        public string ProductName { get; init; } = string.Empty;
+
+        public int SupplierId { get; init ; }
+        public string SupplierName { get; init ;} = string.Empty;
+
+        public string SupplierProductCode {  get; init ;} = string.Empty;
+        public decimal MinOrderQuantity { get; init; }
+        public int LeadTimeDay { get; init; }
+
+       
+
+    }
+}
---
+++ Backend/src/Application/DTOs/Products/Request/Products/ProductsLowStockReadResponse.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.DTOs.Products.Request.Products
+{
+    public sealed record ProductsLowStockReadResponse
+    {
+        //  ProductId,ProductName,LocationId,LocationName, QuantityOnHand, ReorderLevel
+        public int ProductId { get; init; }
+        public string ProductName { get; init; } = string.Empty;
+        public int LocationId { get; init; }
+        public string LocationName { get; init; } = string.Empty;
+        public decimal QuantityOnHand { get; init; }
+        public decimal ReorderLevel { get; init; }
+    }
+}
