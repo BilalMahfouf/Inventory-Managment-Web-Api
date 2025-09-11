e7641868752b925e7fa331287ced13b99d123d32
Bilal <billelmh501@gmail.com>
2025-09-11 23:36:37 +0100

    feat(create Dtos and request valdiation for the ProductImages endpoints)

---
+++ Backend/src/Application/DTOs/Products/Request/ProductImages/ProductImageUploadRequest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.DTOs.Products.Request.ProductImages
+{
+    public sealed record ProductImageUploadRequest
+    {
+        public Stream FileStream { get; set; } = null!;
+        public string FileName { get; set; } = null!;
+        public string? Alt { get; set; }
+        public bool IsPrimary { get; set; }
+    }
+}
---
+++ Backend/src/Application/FluentValidations/Product/ProductImageRequestValidation.cs
@@ -0,0 +1,23 @@
+﻿using Application.DTOs.Products.Request.ProductImages;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.FluentValidations.Product
+{
+    public class ProductImageUploadRequestValidation 
+        : AbstractValidator<ProductImageUploadRequest>
+    {
+        public ProductImageUploadRequestValidation()
+        {
+            RuleFor(e => e.FileStream)
+                .NotNull()
+                .WithMessage("File stream must be provided.")
+                .Must(stream => stream.Length > 0)
+                .WithMessage("File stream cannot be empty.");
+        }
+    }
+}
