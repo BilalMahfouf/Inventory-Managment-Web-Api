150ce9af4e4de1eb859cfb4c5d8d86a10707888b
Bilal <billelmh501@gmail.com>
2025-09-11 23:37:19 +0100

    feat(create the abstraction for the ProductImages endpoints)

---
+++ Backend/src/Application/Abstractions/Services/Product/IProductImageService.cs
@@ -0,0 +1,26 @@
+﻿using Application.DTOs.Products.Request.ProductImages;
+using Application.DTOs.Products.Response.ProductImages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Abstractions.Services.Product
+{
+    public interface IProductImageService
+    {
+        Task<IReadOnlyCollection<ProductImageReadResponse>> GetProductImages(
+            int productId,
+            CancellationToken cancellationToken = default);
+        Task<ProductImageReadResponse> AddProductImageAsync(int productId,
+            ProductImageUploadRequest request,
+            CancellationToken cancellationToken = default);
+        Task DeleteProductImageAsync(int productId,int productImageId,
+            CancellationToken cancellationToken = default);
+        Task<ProductImageReadResponse> UpdateProductImageAsync(int productId
+            , int ProductImageId, CancellationToken cancellationToken = default);
+        Task SetProductImagePrimaryAsync(int productId
+            , int productImageId, CancellationToken cancellationToken = default);
+    }
+}
