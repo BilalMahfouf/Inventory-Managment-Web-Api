2c5a0c31092965070c151edc60f238d1839ce1a5
Bilal <billelmh501@gmail.com>
2025-09-12 14:26:00 +0100

    feat(add ImageStorage Abstraction)

---
+++ Backend/src/Application/Abstractions/Services/Storage/IImageStorageService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Abstractions.Services.Storage
+{
+    public sealed record ImageUploadResponse()
+    {
+        public string StoragePath { get; init; } = string.Empty;
+        public Uri? Uri { get; init; }
+    }
+    public interface IImageStorageService
+    {
+        Task<ImageUploadResponse> UploadAsync(Stream File, string fileName
+           ,string category ,CancellationToken cancellationToken=default);
+        Task<Stream> GetAsync(string storagePath
+            , CancellationToken cancellationToken = default);
+        Task DeleteAsync(string storagePath
+            , CancellationToken cancellationToken = default);
+    }
+}
