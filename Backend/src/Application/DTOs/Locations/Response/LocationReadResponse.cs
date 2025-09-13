bf0b04a951bc70ad30cbb486450749c44e4e4235
Bilal <billelmh501@gmail.com>
2025-09-13 22:27:38 +0100

    feat:create the Locations Endpoints

---
+++ Backend/src/Application/DTOs/Locations/Response/LocationReadResponse.cs
@@ -0,0 +1,18 @@
+﻿using Application.DTOs.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.DTOs.Locations.Response
+{
+    public sealed record LocationReadResponse : BaseDeletableReadResponse
+    {
+        public string Name { get; init; } = null!;
+        public string Address { get; init; } = null!;
+        public bool IsActive { get; init; }
+        public int LocationTypeId { get; init; }
+        public string LocationTypeName { get; init; } = null!;
+    }
+}
---
+++ Backend/src/Application/FluentValidations/Locations/LocationCreateRequestValidator.cs
@@ -0,0 +1,44 @@
+﻿using Application.Abstractions.UnitOfWork;
+using Application.DTOs.Locations.Request;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.FluentValidations.Locations
+{
+    public class LocationCreateRequestValidator : AbstractValidator<LocationCreateRequest>
+    {
+        public LocationCreateRequestValidator(IUnitOfWork uow)
+        {
+            RuleFor(e=>e.Name)
+                .NotEmpty().WithMessage("Name is required.")
+                .MaximumLength(100).WithMessage("Name must not exceed 100 characters.")
+                .MustAsync(async (name, cancellation) =>
+                {
+                    var existingLocation = await uow.Locations.IsExistAsync(
+                        e=>e.Name==name && !e.IsDeleted);
+                    return !existingLocation;
+                }).WithMessage("A location with the same name already exists.");
+
+            RuleFor(e=>e.Address)
+                .NotEmpty().WithMessage("Address is required.")
+                .MaximumLength(200)
+                .WithMessage("Address must not exceed 200 characters.");
+            
+            RuleFor(e=>e.LocationTypeId).GreaterThan(0)
+                .NotEmpty().WithMessage("locationTypeId is required.")
+                .WithMessage("LocationTypeId must be greater than 0.")
+                .MustAsync(async (locationTypeId, cancellation) =>
+                {
+                    var locationType = await uow.LocationTypes.IsExistAsync(
+                        e => e.Id == locationTypeId && !e.IsDeleted);
+                    return !locationType;
+                }).WithMessage("LocationTypeId does not exist.");
+
+        }
+    }
+}
