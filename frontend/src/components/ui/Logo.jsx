b25e03979a6fcd8b1973f1b4eb1b45e0d81e006f
Bilal <billelmh501@gmail.com>
2025-09-15 13:37:53 +0100

    change the logo

--- frontend/src/components/ui/Logo.jsx
+++ frontend/src/components/ui/Logo.jsx
@@ -1,12 +1,12 @@
-import { Lock } from "lucide-react"
+import { Package } from "lucide-react"
 
 
 
 export default function Logo(extraStyle="mx-auto") {
-    const style=`bg-primary-blue w-12 h-12  rounded-xl pt-2 pl-2 mb-3 ${extraStyle}`;
+    const style=`bg-primary-blue w-9 h-9  rounded-xl pt-2 pl-2 mb-3 ${extraStyle}`;
     return (
         <div className={style}>
-               <Lock size={30} color="#ffffff" absoluteStrokeWidth  c/>
+               <Package size={20} color="#ffffff" absoluteStrokeWidth  c/>
             </div>
     )
 }
