2d687722273a81cb1491a00116e7ce6c80bee3b2
Bilal <billelmh501@gmail.com>
2025-09-21 12:41:48 +0100

    feat: create the reqeust and response dots with validation for stock-movements-types endpoints

---
+++ Backend/src/Application/DTOs/StockMovements/Request/StockMovementTypeRequest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.DTOs.StockMovements.Request
+{
+    public sealed record StockMovementTypeRequest
+    {
+        public string Name { get; init; } = null!;
+        public string? Description { get; init; }
+        public byte Direction { get; init; }
+
+    }
+}
---
+++ Backend/src/Application/DTOs/StockMovements/Response/StockMovementTypeReadResponse.cs
@@ -0,0 +1,17 @@
+﻿using Application.DTOs.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.DTOs.StockMovements.Response
+{
+    public sealed record StockMovementTypeReadResponse : BaseDeletableReadResponse
+    {
+        public string Name { get; init; } = null!;
+        public string? Description { get; init; }
+        public string Direction { get; init; } = null!;
+
+    }
+}
