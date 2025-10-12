4f0588c520c53a4e54eac56f76fffd0bf68d73a9
Bilal <billelmh501@gmail.com>
2025-10-12 15:28:44 +0100

    add type column to ProductCategory entity

---
+++ Backend/src/Infrastructure/Migrations/20251012142517_ProductCategories_AddTypeColumn.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class ProductCategories_AddTypeColumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<byte>(
+                name: "Type",
+                table: "ProductCategories",
+                type: "tinyint",
+                nullable: false,
+                defaultValue: (byte)2,
+                comment: "  1 is MainCategory ,2 is SubCategory");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Type",
+                table: "ProductCategories");
+        }
+    }
+}
